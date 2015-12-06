83a902796917cf8d945465e880e59fcab7af181b
Henrik Nyh <henrik@nyh.se>
2015-12-06 22:25:05 +0100

    4B in Elixir

---
+++ day4b.exs
@@ -0,0 +1,18 @@
+defmodule Solution do
+  @input "iwrupvqb"
+
+  # I was going to make this parallel but turns out it was plenty fast anyway…
+  def run do
+    ints
+    |> Enum.find(fn i -> hash(@input <> to_string(i)) |> String.starts_with?("000000") end)
+    |> IO.inspect
+  end
+
+  defp ints, do: Stream.iterate(1, &(&1 + 1))
+
+  defp hash(string) do
+    :crypto.hash(:md5, string) |> Base.encode16
+  end
+end
+
+Solution.run
