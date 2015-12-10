4f89b5ca0a98b84a8ede86145cdc435a01d2d6cb
Henrik Nyh <henrik@nyh.se>
2015-12-10 17:36:26 +0100

    10B in Ruby

---
+++ day10b.rb
@@ -0,0 +1,10 @@
+input = "1113222113"
+
+do_it = -> input {
+  input.chars.chunk { |x| x }.map { |n, list| list.length.to_s + n }.join
+}
+
+value = input
+50.times { value = do_it.call(value) }
+
+puts value.length
