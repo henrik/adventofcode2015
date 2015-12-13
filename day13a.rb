d9b289a24977426ae46505825f202f9fad557c31
Henrik Nyh <henrik@nyh.se>
2015-12-13 11:44:10 +0100

    13A in Ruby

---
+++ day13a.rb
@@ -0,0 +1,98 @@
+# This is very similar to day 9; even the number of nodes (8) and thus the number of possible permutations (8! = 40 320).
+# Since the number is low, we can happily brute-force it.
+
+data = DATA.read.strip
+
+scores = {}
+
+# Get all scores.
+
+data.each_line do |line|
+  if line.match(/(\w+) would lose (\d+) .+ (\w+)\./)
+    name1, loss, name2 = $1, $2.to_i, $3
+    scores[[name1, name2]] = -loss
+  elsif line.match(/(\w+) would gain (\d+) .+ (\w+)\./)
+    name1, gain, name2 = $1, $2.to_i, $3
+    scores[[name1, name2]] = gain
+  else
+    raise "wtf!"
+  end
+end
+
+# Calculate all permutations.
+
+names = scores.keys.flatten.uniq
+
+arrangements = {}
+
+names.permutation.each do |arrangement|
+
+  wrapped_arrangement = arrangement + [ arrangement.first ]
+  score = wrapped_arrangement.each_cons(2).map { |name1, name2|
+    scores.fetch([name1, name2]) + scores.fetch([name2, name1])
+  }.inject(:+)
+
+  arrangements[arrangement] = score
+end
+
+# Find the best one.
+
+puts arrangements.values.max
+
+__END__
+Alice would lose 2 happiness units by sitting next to Bob.
+Alice would lose 62 happiness units by sitting next to Carol.
+Alice would gain 65 happiness units by sitting next to David.
+Alice would gain 21 happiness units by sitting next to Eric.
+Alice would lose 81 happiness units by sitting next to Frank.
+Alice would lose 4 happiness units by sitting next to George.
+Alice would lose 80 happiness units by sitting next to Mallory.
+Bob would gain 93 happiness units by sitting next to Alice.
+Bob would gain 19 happiness units by sitting next to Carol.
+Bob would gain 5 happiness units by sitting next to David.
+Bob would gain 49 happiness units by sitting next to Eric.
+Bob would gain 68 happiness units by sitting next to Frank.
+Bob would gain 23 happiness units by sitting next to George.
+Bob would gain 29 happiness units by sitting next to Mallory.
+Carol would lose 54 happiness units by sitting next to Alice.
+Carol would lose 70 happiness units by sitting next to Bob.
+Carol would lose 37 happiness units by sitting next to David.
+Carol would lose 46 happiness units by sitting next to Eric.
+Carol would gain 33 happiness units by sitting next to Frank.
+Carol would lose 35 happiness units by sitting next to George.
+Carol would gain 10 happiness units by sitting next to Mallory.
+David would gain 43 happiness units by sitting next to Alice.
+David would lose 96 happiness units by sitting next to Bob.
+David would lose 53 happiness units by sitting next to Carol.
+David would lose 30 happiness units by sitting next to Eric.
+David would lose 12 happiness units by sitting next to Frank.
+David would gain 75 happiness units by sitting next to George.
+David would lose 20 happiness units by sitting next to Mallory.
+Eric would gain 8 happiness units by sitting next to Alice.
+Eric would lose 89 happiness units by sitting next to Bob.
+Eric would lose 69 happiness units by sitting next to Carol.
+Eric would lose 34 happiness units by sitting next to David.
+Eric would gain 95 happiness units by sitting next to Frank.
+Eric would gain 34 happiness units by sitting next to George.
+Eric would lose 99 happiness units by sitting next to Mallory.
+Frank would lose 97 happiness units by sitting next to Alice.
+Frank would gain 6 happiness units by sitting next to Bob.
+Frank would lose 9 happiness units by sitting next to Carol.
+Frank would gain 56 happiness units by sitting next to David.
+Frank would lose 17 happiness units by sitting next to Eric.
+Frank would gain 18 happiness units by sitting next to George.
+Frank would lose 56 happiness units by sitting next to Mallory.
+George would gain 45 happiness units by sitting next to Alice.
+George would gain 76 happiness units by sitting next to Bob.
+George would gain 63 happiness units by sitting next to Carol.
+George would gain 54 happiness units by sitting next to David.
+George would gain 54 happiness units by sitting next to Eric.
+George would gain 30 happiness units by sitting next to Frank.
+George would gain 7 happiness units by sitting next to Mallory.
+Mallory would gain 31 happiness units by sitting next to Alice.
+Mallory would lose 32 happiness units by sitting next to Bob.
+Mallory would gain 95 happiness units by sitting next to Carol.
+Mallory would gain 91 happiness units by sitting next to David.
+Mallory would lose 66 happiness units by sitting next to Eric.
+Mallory would lose 75 happiness units by sitting next to Frank.
+Mallory would lose 99 happiness units by sitting next to George.
