ddf96b2e68a958ed7e28e5e8c04b93d7ba790f43
Henrik Nyh <henrik@nyh.se>
2015-12-22 16:30:15 +0100

    20A in Ruby

---
+++ day20a.rb
@@ -0,0 +1,27 @@
+MIN_PRESENTS = 36_000_000
+
+# Naive algorithm. There are faster, more cryptic ones.
+# def factors_of(num)
+#   num.downto(1).select { |n| (num % n).zero? }
+# end
+
+# Ended up using a faster, more cryptic algorithm that I did not write myself:
+# http://stackoverflow.com/a/21165002/6962
+def factors_of(num)
+  1.upto(Math.sqrt(num)).select { |i| (num % i).zero? }.inject([]) do |f, i|
+    f << i
+    f << num / i unless i == num / i
+    f
+  end
+end
+
+def count_for_house(num)
+  factors = factors_of(num)
+  factors.map { |elf| elf * 10 }.inject(:+)
+end
+
+# Arrived at these numbers by trial and error, and replacing a value if Advent of Code said my result was too high…
+# Wanted to use a binary search but that's tricky since the values aren't monotonic.
+p (600_000..856800).find { |i|
+  count_for_house(i) >= MIN_PRESENTS
+}
